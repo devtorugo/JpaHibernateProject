4574affa5b0ec24d710d726105aeec1e1920c69e
VICTOR <victorzarbesu@gmail.com>
2024-05-04 00:28:30 -0300

    mapeando entiddade Produto

---
+++ src/main/java/fiap/ddd/entities/Produto.java
@@ -0,0 +1,8 @@
+package fiap.ddd.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Produto {
+
+}
---
+++ src/test/java/src/main/fiap/ddd/entities/Produto.java
@@ -0,0 +1,4 @@
+package src.main.fiap.ddd.entities;
+
+public class Produto {
+}
