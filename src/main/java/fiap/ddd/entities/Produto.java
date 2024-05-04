3cf4123d5ad12c3838387446c4858729bc64fa47
VICTOR <victorzarbesu@gmail.com>
2024-05-04 00:43:28 -0300

    Criando a entidade de persistência Produto

--- src/main/java/fiap/ddd/entities/Produto.java
+++ src/main/java/fiap/ddd/entities/Produto.java
@@ -1,8 +1,16 @@
 package fiap.ddd.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "tb_produto")
 public class Produto {
+
+    @Id
+    @SequenceGenerator(name = "produto", sequenceName="sq_tb_produto", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto")
+    private int id;
+
+
 
 }
