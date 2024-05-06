c981a7729fdaf72bb3ca6726449f7153abefae27
VICTOR <victorzarbesu@gmail.com>
2024-05-06 19:17:38 -0300

    Criando entidade Contrato Aluguel

---
+++ src/main/java/fiap/ddd/entities/ContratoAluguel.java
@@ -0,0 +1,45 @@
+package fiap.ddd.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Calendar;
+
+@Entity
+@Table(name = "tb_contrato_aluguel")
+public class ContratoAluguel {
+
+    @Id
+    @Column(name = "id_contrato")
+    private int id;
+
+    @Column(name="vl_aluguel")
+    private float valor;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="dt_vencimento_aluguel")
+    private Calendar dataVenciento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Calendar getDataVenciento() {
+        return dataVenciento;
+    }
+
+    public void setDataVenciento(Calendar dataVenciento) {
+        this.dataVenciento = dataVenciento;
+    }
+}
