a6029e38a9104bf98134bd47517e186c8e62d982
VICTOR <victorzarbesu@gmail.com>
2024-05-07 09:54:32 -0300

    Realização de chave composta entre Usuário e Avaliação

---
+++ src/main/java/fiap/ddd/entities/Avaliacao.java
@@ -0,0 +1,48 @@
+package fiap.ddd.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_avaliacao")
+@IdClass(AvaliacaoId.class)
+public class Avaliacao implements Serializable {
+
+    @Id
+    @JoinColumn(name="id_usuario")
+    @ManyToOne(optional = false)
+    private Usuario usuario;
+    @Id
+    @JoinColumn(name = "id_estabelecimento")
+    @ManyToOne(optional = false)
+    private Estabelecimento estabelecimento;
+
+
+    @Column(name="vl_nota")
+    private int nota;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Estabelecimento getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(Estabelecimento estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
---
+++ src/main/java/fiap/ddd/entities/AvaliacaoId.java
@@ -0,0 +1,45 @@
+package fiap.ddd.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AvaliacaoId implements Serializable {
+    private int usuario;
+    private int estabelecimento;
+
+    public AvaliacaoId(){}
+
+    public AvaliacaoId(int usuario, int estabelecimento) {
+        this.usuario = usuario;
+        this.estabelecimento = estabelecimento;
+    }
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(int estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoId that = (AvaliacaoId) o;
+        return usuario == that.usuario && estabelecimento == that.estabelecimento;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuario, estabelecimento);
+    }
+}
